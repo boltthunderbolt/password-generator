4cfb95096c5644c05704d381b022f66cf6162610
Thunder Bolt <bolt.thunderbolt168@gmail.com>
2024-08-11 14:35:20 +0700

    Added strong password

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 - Users can determine the desired character length
 - **Input User Password** input the weakness password to generate
 - Exit Menu program
+- Strong password
--- password/passwordGenerator.py
+++ password/passwordGenerator.py
@@ -1,11 +1,31 @@
 import random
- 
+
+# Kamus leetspeak untuk mengganti karatkter tertentu dengan angka
+leet_dict = {
+  'a': '4', 'A': '4',
+  'e': '3', 'E': '3',
+  'i': '1', 'I': '1',
+  'o': '0', 'O': '0',
+  's': '5', 'S': '5',
+  't': '7', 'T': '7'
+}
+
+def replace_with_leetspeak(user_input):
+  # Mengganti beberapa karakter dalam input user dengan angka menggunakan kamus leetspeak
+  leet_input = ''.join([leet_dict[char] if char in leet_dict else char for char in user_input])
+  return leet_input
+
 def generate_password(length, user_input):
   # Daftar lengkap characters
   characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+[]{}|;:',.<>?/~`"
 
+  # Ganti beberapa karakter dalam input user dalam bentuk angka untuk memperkuat password
+  leet_input = replace_with_leetspeak(user_input)
+  # Password yang telah diubah menjadi leetspeak
+  password = leet_input
+
   # Menggabungkan input user dengan karakter acak dari daftar characters
-  password = user_input
+  # Tambahkan karakter acak jika user menentukan panjang karakter lebih dari password
   while len(password) < length:
     password += random.choice(characters)
   
